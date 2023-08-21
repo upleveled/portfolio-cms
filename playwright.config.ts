e924648275326dd109997b63d41f3f17d24d3918
Lukas Prochazka <prochazka.lu@gmail.com>
2023-08-21 15:07:08 +0200

    Delete next.d.ts, update playwright config yaml

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,32 +1,21 @@
-import { devices, PlaywrightTestConfig } from '@playwright/test';
+import { PlaywrightTestConfig } from '@playwright/test';
 
 // Config file docs: https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'pnpm start',
     port: 3000,
-    reuseExistingServer: !process.env.CI,
   },
-  testIgnore: '**/util/__tests__/**',
-  retries: process.env.CI ? 2 : 0,
+  testMatch: '**/playwright/**',
   reporter: process.env.CI
-    ? 'list'
+    ? 'github'
     : [['html', { outputFolder: 'playwright/report/' }]],
   outputDir: 'playwright/test-results/',
   use: {
-    trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     testIdAttribute: 'data-test-id',
   },
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
-  ],
 };
 
 export default config;
