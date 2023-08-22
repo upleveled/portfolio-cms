d25b3387b0bbfd303346466daa9577237c382a26
Lukas Prochazka <prochazka.lu@gmail.com>
2023-08-22 21:31:51 +0200

    Update navigation test

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -6,4 +6,10 @@ test('check content', async ({ page }) => {
   await expect(page.getByTestId('whoAmI')).toBeVisible();
   await expect(page.getByTestId('myWork')).toBeVisible();
   await expect(page.getByTestId('footer')).toBeVisible();
+
+  await page.getByRole('link', { name: 'My work' }).click();
+  await page.waitForURL('/#portfolio');
+
+  await page.getByRole('link', { name: 'Contact' }).click();
+  await page.waitForURL('/#about');
 });
